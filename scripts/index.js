6650044da8b448f94d29a4c14ce0fbd498c6a8c9
Jonathan Duck <duckbrain30@gmail.com>
2015-03-22 07:07:46 -0700

    Some research into the Journal API and faster page lookups.
    On paper, I have created a new database design that should make things much
    faster now that I know how I will access it. I will place all the data into
    a single "path" objectStore when the items are downloaded.

--- scripts/index.js
+++ scripts/index.js
@@ -1,149 +1,153 @@
 (function() {
 
-    database = new DatabaseModel()
-    var private = {
-        debug: window.debug || true,
-        theme: null,
-        page: null,
-        template: null,
-        contentElement: document.getElementById('main-content')
-    };
+	database = new DatabaseModel();
+	database.path = new PathCacheModel(database, database.path);
 
-    function displayPage(info) {
-        console.log(info);
-        private.page = info;
-        private.contentElement.innerHTML = private.template.render({
-            lang: getUrlParameter('lang'),
-            info: info,
-            _: getI18nMessage
-        });
-        attachLinks('a[data-info]', onLinkClicked);
-        return info;
-        //return prefetch(info);
-    }
-    
-    function attachLinks(query, handler) {
-        var links = document.querySelectorAll(query);
-        for (var i = 0; i < links.length; i++) {
-            links[i].addEventListener('click', handler);
-        }
-    }
-    
-    function onLinkClicked(e) {
-        console.log(e);
-        var info = JSON.parse(e.target.dataset.info);
-        database.path.getDetails(info).then(displayPage);
-        console.log(info);
-        
-        e.preventDefault();
-        return false;
-    }
-    
-    function onRefrenceClicked(e) {
-        
-    }
+	var private = {
+		debug: window.debug || true,
+		theme: null,
+		page: null,
+		template: null,
+		contentElement: document.getElementById('main-content')
+	};
 
-    function prefetch(info) {
-        
-        function clean(i) {
-            if (!i) {
-                return i;
-            }
-            delete i['content'];
-            delete i['refrences'];
-            if (i.children && i.heiarchy) {
-                i.children = i.children.map(clean);
-                i.heiarchy = i.heiarchy.map(clean);
-                i.next = clean(i.next);
-                i.previous = clean(i.previous);
-                i.up = clean(i.up);
-            }
-            return i;
-        }
-        
-        function mapDetails(array) {
-            if (!array) {
-                return Promise.resolve(array);
-            }
-            return Promise.all(array.map(function(e) {
-                return database.path.getDetails(e).then(clean);
-            }));
+	function displayPage(info) {
+		console.log(info);
+		private.page = info;
+		private.contentElement.innerHTML = private.template.render({
+			lang: getUrlParameter('lang'),
+			info: info,
+			_: getI18nMessage
+		});
+		attachLinks('a[data-info]', onLinkClicked);
+		return info;
+		//return prefetch(info);
+	}
 
-        }
-        
-        return Promise.all([
-                            database.path.getDetails(info.previous).then(clean),
-                            database.path.getDetails(info.next).then(clean),
-                            mapDetails(info.children),
-                            mapDetails(info.heiarchy)
-                ]).then(function(e) {
-                    info.previous = e[0];
-                    info.next = e[1];
-                    info.children = e[2];
-                    info.heiarchy = e[3];
-                    return info;
-                });
-    }
+	function attachLinks(query, handler) {
+		var links = document.querySelectorAll(query);
+		for (var i = 0; i < links.length; i++) {
+			links[i].addEventListener('click', handler);
+		}
+	}
 
-    function loadTheme(theme) {
-        private.theme = theme;
-        private.template = new EJS({
-            text: private.theme.template
-        });
-    }
+	function onLinkClicked(e) {
+		console.log(e);
+		var info = JSON.parse(e.target.dataset.info);
+		database.path.getDetails(info).then(displayPage);
+		console.log(info);
 
-    function getUrlParameter(name) {
-        return decodeURIComponent((new RegExp('[?|&]' + name + '=' +
-                '([^&;]+?)(&|#|;|$)').exec(location.search) || [ , "" ])[1]
-                .replace(/\+/g, '%20')) ||
-                null
-    }
+		e.preventDefault();
+		return false;
+	}
 
-    function getPathInfo() {
-        return {
-            query: getUrlParameter('q') || '/',
-            lang: getUrlParameter('lang'),
-            ref: getUrlParameter('ref')
-        }
-    }
+	function onRefrenceClicked(e) {
 
-    function getI18nMessage(name, params) {
-        var message = chrome.i18n.getMessage(name, params);
-        if (!message) {
-            return name;
-        }
-        return message;
-    }
+	}
 
-    var pathInfo = getPathInfo();
-    database.open().then(
-            function() {
-                return Promise.all([ pathInfo.lang ? database.language
-                        .getByLdsCode(pathInfo.lang) : database.settings
-                        .getLanguage().then(database.language.get),
-                        database.settings.getAll() ]);
-            }).then(
-            function(e) {
-                var language = e[0];
-                var settings = e[1];
-                return Promise.all([ database.path.get(language.id,
-                        pathInfo.query).then(database.path.getDetails),
-                        database.theme.get(settings.theme) ]);
-            }).then(function(e) {
-        loadTheme(e[1]);
-        return displayPage(e[0]);
-    });
+	function prefetch(info) {
 
-    if (private.debug) {
-        window.debug = private;
-        window.database = database;
-        private.displayPage = displayPage;
-        private.getPathInfo = getPathInfo;
-        window.log = function log(e) {
-            console.log(e);
-            return e;
-        };
-    }
+		function clean(i) {
+			if (!i) {
+				return i;
+			}
+			delete i['content'];
+			delete i['refrences'];
+			if (i.children && i.heiarchy) {
+				i.children = i.children.map(clean);
+				i.heiarchy = i.heiarchy.map(clean);
+				i.next = clean(i.next);
+				i.previous = clean(i.previous);
+				i.up = clean(i.up);
+			}
+			return i;
+		}
 
-    window.displayPage = displayPage;
+		function mapDetails(array) {
+			if (!array) {
+				return Promise.resolve(array);
+			}
+			return Promise.all(array.map(function(e) {
+				return database.path.getDetails(e).then(clean);
+			}));
+
+		}
+
+		return Promise.all([
+			database.path.getDetails(info.previous).then(clean),
+			database.path.getDetails(info.next).then(clean),
+			mapDetails(info.children),
+			mapDetails(info.heiarchy)
+		]).then(function(e) {
+			info.previous = e[0];
+			info.next = e[1];
+			info.children = e[2];
+			info.heiarchy = e[3];
+			return info;
+		});
+	}
+
+	function loadTheme(theme) {
+		private.theme = theme;
+		private.template = new EJS({
+			text: private.theme.template
+		});
+	}
+
+	function getUrlParameter(name) {
+		return decodeURIComponent((new RegExp('[?|&]' + name + '=' +
+					'([^&;]+?)(&|#|;|$)').exec(location.search) || [, ""])[1]
+				.replace(/\+/g, '%20')) ||
+			null
+	}
+
+	function getPathInfo() {
+		return {
+			query: getUrlParameter('q') || '/',
+			lang: getUrlParameter('lang'),
+			ref: getUrlParameter('ref')
+		}
+	}
+
+	function getI18nMessage(name, params) {
+		var message = chrome.i18n.getMessage(name, params);
+		if (!message) {
+			return name;
+		}
+		return message;
+	}
+
+	var pathInfo = getPathInfo();
+	database.open().then(
+		function() {
+			return Promise.all([pathInfo.lang ? database.language
+				.getByLdsCode(pathInfo.lang) : database.settings
+				.getLanguage().then(database.language.get),
+				database.settings.getAll()
+			]);
+		}).then(
+		function(e) {
+			var language = e[0];
+			var settings = e[1];
+			return Promise.all([database.path.get(language.id,
+					pathInfo.query).then(database.path.getDetails),
+				database.theme.get(settings.theme)
+			]);
+		}).then(function(e) {
+		loadTheme(e[1]);
+		return displayPage(e[0]);
+	});
+
+	if (private.debug) {
+		window.debug = private;
+		window.database = database;
+		private.displayPage = displayPage;
+		private.getPathInfo = getPathInfo;
+		window.log = function log(e) {
+			console.log(e);
+			return e;
+		};
+	}
+
+	window.displayPage = displayPage;
 })();
--- scripts/models/DatabaseModel.js
+++ scripts/models/DatabaseModel.js
@@ -1,122 +1,160 @@
 /**
  * @name DatabaseModel
  * @class
- * 
+ *
  * @param {LDSContentProvider} contentProvider
  */
 function DatabaseModel(contentProvider) {
-    var that = this;
-    
-    that.server = null;
-    that.connection = {
-        server: 'duck.jonathan.lds-scriptures',
-        version: 12,
-        schema: {
-            languages: {
-                key: { keyPath: 'id' },
-                indexes: {
-                    code: { unique: true },
-                    code_three: { }
-                }
-            },
-            settings: {
-                key: { keyPath: 'id' },
-                indexes: { 'id': {} }
-            },
-            themes: {
-				key: { keyPath: 'id', autoIncrement: true },
+	var that = this;
+
+	that.server = null;
+	that.connection = {
+		server: 'duck.jonathan.lds-scriptures',
+		version: 13,
+		schema: {
+			languages: {
+				key: {
+					keyPath: 'id'
+				},
+				indexes: {
+					code: {
+						unique: true
+					},
+					code_three: {}
+				}
 			},
-            /**
-             * Represents the root catalog for any given language
-             */
-            catalogs: {
-                key: { keyPath: 'languageId' }
-            },
-            nodes: {
-                key: { keyPath: [ 'languageId', 'bookId', 'id' ] },
-                indexes: {
-                    children: { key: [ 'languageId', 'bookId', 'parentId' ] },
-                    path: { key: [ 'languageId', 'path' ], unique: false }
-                }
-            },
-            books: {
-                key: { keyPath: [ 'languageId', 'id' ] },
-                indexes: {
-                    children: { key: [ 'languageId', 'parentFolderId' ] },
-                    path: { key: [ 'languageId', 'path' ], unique: false }
-                }
-            },
-            folders: {
-                key: { keyPath: [ 'languageId', 'id' ] },
-                indexes: {
-                    children: { key: [ 'languageId', 'parentId' ] },
-                    name: { key: [ 'languageId', 'name' ] }
-                }
-            }
-        }
-    };
+			settings: {
+				key: {
+					keyPath: 'id'
+				},
+				indexes: {
+					'id': {}
+				}
+			},
+			themes: {
+				key: {
+					keyPath: 'id',
+					autoIncrement: true
+				},
+			},
+			/**
+			 * Represents the root catalog for any given language
+			 */
+			catalogs: {
+				key: {
+					keyPath: 'languageId'
+				}
+			},
+			nodes: {
+				key: {
+					keyPath: ['languageId', 'bookId', 'id']
+				},
+				indexes: {
+					children: {
+						key: ['languageId', 'bookId', 'parentId']
+					},
+					path: {
+						key: ['languageId', 'path'],
+						unique: false
+					}
+				}
+			},
+			books: {
+				key: {
+					keyPath: ['languageId', 'id']
+				},
+				indexes: {
+					children: {
+						key: ['languageId', 'parentFolderId']
+					},
+					path: {
+						key: ['languageId', 'path'],
+						unique: false
+					}
+				}
+			},
+			folders: {
+				key: {
+					keyPath: ['languageId', 'id']
+				},
+				indexes: {
+					children: {
+						key: ['languageId', 'parentId']
+					},
+					name: {
+						key: ['languageId', 'name']
+					}
+				}
+			},
+			paths: {
+				key: {
+					keyPath: ['languageId', 'path']
+				}
+			}
+		}
+	};
 
-    that.contentProvider = contentProvider;
-    that.helpers = DatabaseModel.helpers;
-    
-    function tryLoadModel(name, className) {
-        // TODO: Use require if in Node.js
+	that.contentProvider = contentProvider;
+	that.helpers = DatabaseModel.helpers;
 
-        // class name is passed, or made by appending "Model" to the name
-        var $class = window[className || name + "Model"];
-        // property name is the name with the first letter lower-case.
-        var propertyName = name.charAt(0).toLowerCase() + name.slice(1);
-        if ($class) {
-            that[propertyName] = new $class(that);
-        }
-    }
-    
-    tryLoadModel('Settings');
-    tryLoadModel('Language');
-    tryLoadModel('Catalog');
-    tryLoadModel('Node');
-    tryLoadModel('Book');
-    tryLoadModel('Folder');
-    tryLoadModel('Path');
-    tryLoadModel('Theme');
-    tryLoadModel('Downloader', 'BrowserDownloader');
-    tryLoadModel('ContentProvider', 'LDSContentProvider');
+	function tryLoadModel(name, className) {
+		// TODO: Use require if in Node.js
 
-    that.open = function open() {
-        return db.open(that.connection).then(function(server) {
-            that.server = server;
-            return that;
-        });
-    };
+		// class name is passed, or made by appending "Model" to the name
+		var $class = window[className || name + "Model"];
+		// property name is the name with the first letter lower-case.
+		var propertyName = name.charAt(0).toLowerCase() + name.slice(1);
+		if ($class) {
+			that[propertyName] = new $class(that);
+		}
+	}
 
-    that.close = function close() {
-        return that.server.close().then(function() {
-            that.server = null;
-            return true;
-        });
-        
-    };
+	tryLoadModel('Settings');
+	tryLoadModel('Language');
+	tryLoadModel('Catalog');
+	tryLoadModel('Node');
+	tryLoadModel('Book');
+	tryLoadModel('Folder');
+	tryLoadModel('Path');
+	tryLoadModel('Theme');
+	tryLoadModel('Downloader', 'BrowserDownloader');
+	tryLoadModel('ContentProvider', 'LDSContentProvider');
+
+	that.open = function open() {
+		return db.open(that.connection).then(function(server) {
+			that.server = server;
+			return that;
+		});
+	};
+
+	that.close = function close() {
+		return that.server.close().then(function() {
+			that.server = null;
+			return true;
+		});
+
+	};
 }
 
 DatabaseModel.helpers = {
-    existsSingle: function existsSingle(promiseAttempt) {
-        // TODO: Return a promise that returns true or false
-    },
-    single: function single(list) {
-        if (list.length > 1) {
-            throw "single had more than one.";
-        }
-        return list[0];
-    },
-    dataToIdArray: function dataToIdArray(array) {
-        var newArray = [ ];
-        for (var i = 0; i < array.length; i++) {
-            newArray.push(array[i].id);
-        }
-        return newArray;
-    }
+	existsSingle: function existsSingle(promiseAttempt) {
+		// TODO: Return a promise that returns true or false
+	},
+	single: function single(list) {
+		if (list.length > 1) {
+			throw "single had more than one.";
+		}
+		return list[0];
+	},
+	dataToIdArray: function dataToIdArray(array) {
+		var newArray = [];
+		for (var i = 0; i < array.length; i++) {
+			newArray.push(array[i].id);
+		}
+		return newArray;
+	}
 }
 
 if (typeof module != 'undefined') {
-    module.exports = DatabaseModel;
+	module.exports = DatabaseModel;
 }
