2a7184e6fe428180fea91ed8511079baf1d2d9d9
Jonathan Duck <duckbrain30@gmail.com>
2015-04-05 23:42:15 -0700

    More work on Default theme.

--- scripts/index.js
+++ scripts/index.js
@@ -1,236 +1,239 @@
 (function() {
 
-  database = new DatabaseModel();
-  database.download = new DatabaseQuery().download;
+	database = new DatabaseModel();
+	database.download = new DatabaseQuery().download;
 
-  if (!String.prototype.endsWith) {
-    String.prototype.endsWith = function(searchString, position) {
-      var subjectString = this.toString();
-      if (position === undefined || position > subjectString.length) {
-        position = subjectString.length;
-      }
-      position -= searchString.length;
-      var lastIndex = subjectString.indexOf(searchString, position);
-      return lastIndex !== -1 && lastIndex === position;
-    };
-  }
+	if (!String.prototype.endsWith) {
+		String.prototype.endsWith = function(searchString, position) {
+			var subjectString = this.toString();
+			if (position === undefined || position > subjectString.length) {
+				position = subjectString.length;
+			}
+			position -= searchString.length;
+			var lastIndex = subjectString.indexOf(searchString, position);
+			return lastIndex !== -1 && lastIndex === position;
+		};
+	}
 
-  var private = {
-    debug: window.debug || true,
-    // An object that contains all of the information, outside the database and
-    // the theme, on how to display the page
-    configuration: null,
-    // An interchangable theme that will generate the final HTML and provides
-    // CSS and JavaScript to help the page
-    theme: null,
-    // The database object of the path to display
-    page: null,
-    // An EJS template from the theme to generate the HTML with.
-    template: null,
-    historyCaused: false,
-    elements: {
-      body: document.body,
-      content: document.getElementById('main-content'),
-      refrences: null
-    }
-  };
-  window.private = private;
+	var private = {
+		debug: window.debug || true,
+		// An object that contains all of the information, outside the database and
+		// the theme, on how to display the page
+		configuration: null,
+		// An interchangable theme that will generate the final HTML and provides
+		// CSS and JavaScript to help the page
+		theme: null,
+		// The database object of the path to display
+		page: null,
+		// An EJS template from the theme to generate the HTML with.
+		template: null,
+		historyCaused: false,
+		elements: {
+			body: document.body,
+			content: document.getElementById('main-content'),
+			refrences: null
+		}
+	};
+	window.private = private;
 
-  function displayPage(info) {
-    if (!private.historyCaused) {
-      if (info) {
-        history.pushState(info, info.name, "index.html?" + info.path);
-      }
-    }
-    var conf = private.configuration;
-    var ele = private.elements;
-    var d = info.details;
-    var needsDownload = info.type == 'book'
-      ? !d.downloadedVersion || d.downloadedVersion < d.catalogVersion
-      : false;
+	function displayPage(info) {
+		if (!private.historyCaused) {
+			if (info) {
+				history.pushState(info, info.name, "index.html?" + info.path);
+			}
+		}
+		var conf = private.configuration;
+		var ele = private.elements;
+		var d = info.details;
+		var needsDownload = info.type == 'book' ? !d.downloadedVersion || d.downloadedVersion < d.catalogVersion : false;
 
-    console.log(info);
-    private.page = info;
+		console.log(info);
+		private.page = info;
 
-    ele.content.innerHTML = private.template.render({
-      page: {
-        configuration: conf,
-        path: info,
-        getI18nMessage: getI18nMessage,
-        languages: private.languages,
-        generator: new HtmlGenerator(conf, getI18nMessage),
-        loading: needsDownload ? getI18nMessage('downloading') : false
-      }
-    });
-    attachLinks('a[data-path]', onLinkClicked);
-    attachLinks('.content a[href]', onRefrenceClicked);
-    attachLinks('.refrences-close', onRefrenceClosedClicked);
-    attachLinks('.refrences a[href]', onFootnoteClicked);
-    ele.refrences = document.querySelector('.refrences')
+		document.title = getI18nMessage('app_title') + ' - ' + info.name;
+		ele.content.innerHTML = private.template.render({
+			page: {
+				configuration: conf,
+				path: info,
+				getI18nMessage: getI18nMessage,
+				languages: private.languages,
+				generator: new HtmlGenerator(conf, getI18nMessage),
+				loading: needsDownload ? getI18nMessage('downloading') : false
+			}
+		});
+		attachLinks('a[data-path]', onLinkClicked);
+		attachLinks('.content a[href]', onRefrenceClicked);
+		attachLinks('.refrences-close', onRefrenceClosedClicked);
+		attachLinks('.refrences a[href]', onFootnoteClicked);
+		ele.refrences = document.querySelector('.refrences')
 
-    // Begin downloading book if not up to day. The template can similarly check if this is needed
-    if (needsDownload) {
-      return database.download.downloadBook(info.id).then(function() {
-        return database.path.get(info.id).then(displayPage);
-      });
-    }
+		// Begin downloading book if not up to day. The template can similarly check if this is needed
+		if (needsDownload) {
+			return database.download.downloadBook(info.id).then(function() {
+				return database.path.get(info.id).then(displayPage);
+			});
+		}
 
-    return info;
-  }
+		return info;
+	}
 
-  function attachLinks(query, handler) {
-    var links = document.querySelectorAll(query);
-    for (var i = 0; i < links.length; i++) {
-      links[i].addEventListener('click', function(e) {
-        e.preventDefault();
-        handler(e);
-        return false;
-      });
-    }
-  }
+	function attachLinks(query, handler) {
+		var links = document.querySelectorAll(query);
+		for (var i = 0; i < links.length; i++) {
+			links[i].addEventListener('click', function(e) {
+				e.preventDefault();
+				handler(e);
+				return false;
+			});
+		}
+	}
 
-  function onLinkClicked(e) {
-    var id = parseInt(e.target.dataset.path);
-    database.path.get(id).then(displayPage);
-  }
+	function onLinkClicked(e) {
+		var id = parseInt(e.target.dataset.path);
+		database.path.get(id).then(displayPage);
+	}
 
-  function onRefrenceClicked(e) {
-    var body = document.body;
-    var refrence = e.target.hash.substring(1);
-    var priorRefrences = document.querySelectorAll('.refrences .selected');
-    var refrenceDiv = document.getElementById(refrence);
-    if (!body.className.match(/(?:^|\s)refrences-open(?!\S)/g)) {
-      body.className += ' refrences-open';
-    }
-    refrenceDiv.className = 'selected';
-    for (var i = 0; i < priorRefrences.length; i++) {
-      priorRefrences[i].className = '';
-    }
-    private.elements.refrences.scrollTop = refrenceDiv.offsetTop;
-  }
+	function onRefrenceClicked(e) {
+		//TODO: There are many links in the scripture content that do not refer to a footnote, but to another scripture
+		// directly. The two types of links should be sent to one common function and dispatched from there. The event
+		// listeners should all be for that dispatching function.
 
-  function onFootnoteClicked(e) {
-    getConfiguration(e.target.pathname).then(function(conf) {
-      return database.path.getPath(conf.language, conf.path).then(displayPage);
-    });
-  }
+		var body = document.body;
+		var refrence = e.target.hash.substring(1);
+		var priorRefrences = document.querySelectorAll('.refrences .selected');
+		var refrenceDiv = document.getElementById(refrence);
+		if (!body.className.match(/(?:^|\s)refrences-open(?!\S)/g)) {
+			body.className += ' refrences-open';
+		}
+		refrenceDiv.className = 'selected';
+		for (var i = 0; i < priorRefrences.length; i++) {
+			priorRefrences[i].className = '';
+		}
+		private.elements.refrences.scrollTop = refrenceDiv.offsetTop;
+	}
 
-  function onRefrenceClosedClicked(e) {
-    var body = document.body;
-    body.className = body.className.replace(/(?:^|\s)refrences-open(?!\S)/g, '');
-  }
+	function onFootnoteClicked(e) {
+		getConfiguration(e.target.pathname).then(function(conf) {
+			return database.path.getPath(conf.language, conf.path).then(displayPage);
+		});
+	}
 
-  function onLanguageSelected(e) {
+	function onRefrenceClosedClicked(e) {
+		var body = document.body;
+		body.className = body.className.replace(/(?:^|\s)refrences-open(?!\S)/g, '');
+	}
 
-  }
+	function onLanguageSelected(e) {
 
-  function loadTheme(theme) {
-    console.log("Theme: ", theme)
-    private.theme = theme;
-    private.template = new EJS({
-      text: theme.template
-    });
-    less.render(theme.style, {
-      globalVars: {}
-    }).then(function(output) {
-      document.getElementById('custom-css').innerHTML = output.css;
-    });
-  }
+	}
 
-  function getUrlParameter(name, search) {
-    search = (typeof search == 'string') ? search : location.search;
-    return decodeURIComponent((new RegExp('[?|&]' + name + '=' +
-          '([^&;]+?)(&|#|;|$)').exec(location.search) || [, ""])[1]
-        .replace(/\+/g, '%20')) ||
-      null
-  }
+	function loadTheme(theme) {
+		console.log("Theme: ", theme)
+		private.theme = theme;
+		private.template = new EJS({
+			text: theme.template
+		});
+		less.render(theme.style, {
+			globalVars: private.configuration.themeOptions
+		}).then(function(output) {
+			document.getElementById('custom-css').innerHTML = output.css;
+		});
+	}
 
-  window.addEventListener('popstate', function() {
-    private.historyCaused = true;
-    displayPage(history.state);
-    private.historyCaused = false;
-  });
+	function getUrlParameter(name, search) {
+		search = (typeof search == 'string') ? search : location.search;
+		return decodeURIComponent((new RegExp('[?|&]' + name + '=' +
+					'([^&;]+?)(&|#|;|$)').exec(location.search) || [, ""])[1]
+				.replace(/\+/g, '%20')) ||
+			null
+	}
 
-  function getI18nMessage(name, params) {
-    var message = chrome.i18n.getMessage(name, params);
-    if (!message) {
-      return name;
-    }
-    return message;
-  }
+	window.addEventListener('popstate', function() {
+		private.historyCaused = true;
+		displayPage(history.state);
+		private.historyCaused = false;
+	});
 
-  function getConfiguration(search) {
-    search = (typeof search == 'string') ? search : location.search.substring(1);
-    var param = getUrlParameter;
-    return Promise.all([database.settings.getAll(),
-        database.language.getAll()
-      ])
-      .then(function(e) {
-        var path, conf;
+	function getI18nMessage(name, params) {
+		var message = chrome.i18n.getMessage(name, params);
+		if (!message) {
+			return name;
+		}
+		return message;
+	}
 
-        conf = e[0];
-        if (search.lastIndexOf('?') != -1) {
-          path = search.substring(0, search.lastIndexOf('?')).split('.');
-          search = search.substring(search.lastIndexOf('?'))
-        } else {
-          path = search.split('.');
-          search = '';
-        }
-        conf.path = path[0];
-        conf.verses = path;
-        conf.verses.shift(1);
-        conf.language = parseInt(param('lang', search)) || conf.language;
-        //if (conf.path.indexOf('/', conf.path.length - 1) !== -1) {
-        //  conf.path = conf.path.substring(0, conf.path.length - 1);
-        //}
-        //conf.reference = param('ref') || null;
-        //TODO: Parse the refrences
+	function getConfiguration(search) {
+		search = (typeof search == 'string') ? search : location.search.substring(1);
+		var param = getUrlParameter;
+		return Promise.all([database.settings.getAll(),
+				database.language.getAll()
+			])
+			.then(function(e) {
+				var path, conf;
 
-        private.configuration = conf;
-        private.languages = e[1];
-        return conf;
-      });
-  }
+				conf = e[0];
+				if (search.lastIndexOf('?') != -1) {
+					path = search.substring(0, search.lastIndexOf('?')).split('.');
+					search = search.substring(search.lastIndexOf('?'))
+				} else {
+					path = search.split('.');
+					search = '';
+				}
+				conf.path = path[0];
+				conf.verses = path;
+				conf.verses.shift(1);
+				conf.language = parseInt(param('lang', search)) || conf.language;
+				//if (conf.path.indexOf('/', conf.path.length - 1) !== -1) {
+				//  conf.path = conf.path.substring(0, conf.path.length - 1);
+				//}
+				//conf.reference = param('ref') || null;
+				//TODO: Parse the refrences
 
-  function startPage() {
-    return database.open()
-      .then(getConfiguration)
-      .then(function(conf) {
-        return Promise.all([
-          database.path.getPath(conf.language, conf.path),
-          database.theme.get(conf.theme)
-        ]);
-      }).then(function(e) {
-        var lang = private.configuration.language;
-        // TODO: Verify the theme loaded correctly, then default to "default"
-        loadTheme(e[1]);
+				private.configuration = conf;
+				private.languages = e[1];
+				return conf;
+			});
+	}
 
-        var path = e[0];
-        if (!path) {
-          return database.path.getPath(lang, '/').then(function(catalogRoot) {
-            if (!catalogRoot) {
-              //The catalog has not been downloaded. Lets do it.
-              //TODO: Display Loading screen
-              return database.download.downloadCatalog(lang).then(startPage);
-            } else {
-              //TODO: Navigate up the path and redirect
+	function startPage() {
+		return database.open()
+			.then(getConfiguration)
+			.then(function(conf) {
+				return Promise.all([
+					database.path.getPath(conf.language, conf.path),
+					database.theme.get(conf.theme)
+				]);
+			}).then(function(e) {
+				var lang = private.configuration.language;
+				// TODO: Verify the theme loaded correctly, then default to "default"
+				loadTheme(e[1]);
 
-            }
-          });
-        } else {
-          return displayPage(e[0]);
-        }
-      });
-  }
+				var path = e[0];
+				if (!path) {
+					return database.path.getPath(lang, '/').then(function(catalogRoot) {
+						if (!catalogRoot) {
+							//The catalog has not been downloaded. Lets do it.
+							//TODO: Display Loading screen
+							return database.download.downloadCatalog(lang).then(startPage);
+						} else {
+							//TODO: Navigate up the path and redirect
 
-  startPage();
+						}
+					});
+				} else {
+					return displayPage(e[0]);
+				}
+			});
+	}
 
-  if (private.debug) {
-    window.debug = private;
-    window.database = database;
-    window.log = function log(e) {
-      console.log(e);
-      return e;
-    };
-  }
+	startPage();
+
+	if (private.debug) {
+		window.debug = private;
+		window.database = database;
+		window.log = function log(e) {
+			console.log(e);
+			return e;
+		};
+	}
 })();
--- scripts/models/LDSZBookInstaller.js
+++ scripts/models/LDSZBookInstaller.js
@@ -1,102 +1,103 @@
 function LDSZBookInstaller(db, sqlitedb, book) {
-	var that = this;
-	var nodes;
-	var helpers = new LDSInstallerHelpers(db);
+  var that = this;
+  var nodes;
+  var helpers = new LDSInstallerHelpers(db);
 
-	function install() {
-		nodes = {};
+  function install() {
+    nodes = {};
 
-		var result = sqlitedb.exec(
-			'SELECT id, parent_id, title, subtitle, ' +
-			'short_title, uri, content, refs ' +
-			'FROM nodes'
-		);
+    var result = sqlitedb.exec(
+      'SELECT id, parent_id, title, subtitle, ' +
+      'short_title, uri, content, refs ' +
+      'FROM nodes'
+    );
 
-		return addAll(result[0].values)
-			.then(findTree)
-			.then(findRelations)
-			.then(updateVersion)
-			.then(helpers.update);
-		// TODO: Update the book's version
-		var metadata = sqlitedb.exec('SELECT version FROM bookmeta')[0];
-	}
+    return addAll(result[0].values)
+      .then(findTree)
+      .then(findRelations)
+      .then(updateVersion)
+      .then(helpers.update);
+    // TODO: After updated, it should look for the book or nodes that have 1 child and no content. Those items should
+    // be updated to have thier child's name and content, but the names on refrences should not be updated. The child
+    // can then be deleted.
+  }
 
-	function updateVersion() {
-		book.details.downloadedVersion = book.details.catalogVersion;
-		return book;
-	}
+  function updateVersion() {
+    book.details.downloadedVersion = book.details.catalogVersion;
+    return book;
+  }
 
-	function uninstall() {
-		//TODO
-	}
+  function uninstall() {
+    //TODO
+  }
 
-	function addAll(rows) {
-		return Promise.all(rows.map(add));
-	}
+  function addAll(rows) {
+    return Promise.all(rows.map(add));
+  }
 
-	function add(glNode) {
-		var item = formatNode(glNode);
+  function add(glNode) {
+    var item = formatNode(glNode);
 
-		return db.add(item).then(function(item) {
-			item = item[0]; // Item comes back as an array with one item
-			nodes[glNode[0]] = item;
-		});
-	}
+    return db.add(item).then(function(item) {
+      item = item[0]; // Item comes back as an array with one item
+      nodes[glNode[0]] = item;
+    });
+  }
 
-	function findTree() {
-		var node, parent, i;
+  function findTree() {
+    var node, parent, i;
 
-		book.children = [];
+    book.children = [];
 
-		for (i in nodes) {
-			node = nodes[i];
-			parent = nodes[node.parent];
-			if (parent) {
-				node.parent = parent;
-				parent.children.push(node);
-			} else {
-				node.parent = book;
-				book.children.push(node);
-			}
-		}
+    for (i in nodes) {
+      node = nodes[i];
+      parent = nodes[node.parent];
+      if (parent) {
+        node.parent = parent;
+        parent.children.push(node);
+      } else {
+        node.parent = book;
+        book.children.push(node);
+      }
+    }
 
-		return book;
-	}
+    return book;
+  }
 
-	function findRelations(node) {
-		node.heiarchy = node.parent.heiarchy.concat([node]);
-		node.next = helpers.findSibling(+1, node);
-		node.previous = helpers.findSibling(-1, node);
-		for (var i = 0; i < node.children.length; i++) {
-			findRelations(node.children[i]);
-		}
-		return node;
-	}
+  function findRelations(node) {
+    node.heiarchy = node.parent.heiarchy.concat([node]);
+    node.next = helpers.findSibling(+1, node);
+    node.previous = helpers.findSibling(-1, node);
+    for (var i = 0; i < node.children.length; i++) {
+      findRelations(node.children[i]);
+    }
+    return node;
+  }
 
-	function formatNode(row) {
-		return {
-			languageId: book.languageId,
-			parent: row[1],
-			path: row[5],
-			next: null,
-			previous: null,
-			children: [],
-			name: row[2],
-			type: 'node',
-			media: [],
-			heiarchy: [],
-			details: {
-				content: row[6],
-				refrences: row[7],
-				shortTitle: row[4],
-				subtitle: row[3]
-			}
-		};
-	}
+  function formatNode(row) {
+    return {
+      languageId: book.languageId,
+      parent: row[1],
+      path: row[5],
+      next: null,
+      previous: null,
+      children: [],
+      name: row[2],
+      type: 'node',
+      media: [],
+      heiarchy: [],
+      details: {
+        content: row[6],
+        refrences: row[7],
+        shortTitle: row[4],
+        subtitle: row[3]
+      }
+    };
+  }
 
-	that.install = install;
+  that.install = install;
 }
 
 if (typeof module != 'undefined') {
-	module.exports = LDSZBookInstaller;
+  module.exports = LDSZBookInstaller;
 }
