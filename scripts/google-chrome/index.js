a92f687b57356bbce7095ed17e66895b335f2248
Jonathan Duck <duckbrain30@gmail.com>
2015-05-29 18:41:29 -0700

    Create duplicate of index.js and index.html for Chrome Extension Specific. This allows the server code to be uninterrupted and the Chrome Extension can behave slightly differently.

---
+++ scripts/google-chrome/index.js
@@ -0,0 +1,26 @@
+(function () {
+	//var database, navigation;
+
+	function getI18nMessage(name, params) {
+		return name;
+		if (typeof name != 'string') {
+			return name;
+		}
+		return chrome.i18n.getMessage(name, params) || name;
+	}
+
+	database = new DatabaseModel();
+	database.download = new DatabaseQuery(new ChromeMessageProvider()).download;
+	database.contentProvider = new LDSContentProvider(database);
+	navigation = new NavigationController(database);
+
+	navigation.loadPath(location.href);
+	navigation.getI18nMessage = getI18nMessage;
+
+	database.open().then(navigation.init).then(navigation.navigateLoaded);
+})();
+
+function log(debug) {
+	console.log(debug);
+	return debug;
+}
