a864f2dc62f775407cf449f944d5bc6f3996cc67
Jonathan Duck <duckbrain30@gmail.com>
2015-06-05 07:51:25 -0700

    Converted Legacy app to Chrome App

--- scripts/google-chrome/background.js
+++ scripts/google-chrome/background.js
@@ -54,3 +54,12 @@ messageProvider.on('miss', function (e, sender) {
 		sender: sender
 	};
 });
+
+chrome.app.runtime.onLaunched.addListener(function() {
+	chrome.app.window.create('index.html', {
+		bounds: {
+			width: 400,
+			height: 500
+		}
+	});
+});
