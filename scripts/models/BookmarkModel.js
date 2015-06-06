cb54431e235f4be65bf76a8768033a5f64b4ced3
Jonathan Duck <duckbrain30@gmail.com>
2015-06-06 08:10:34 -0700

    Rename delete to destroy to handle keyword conflict.

--- scripts/models/BookmarkModel.js
+++ scripts/models/BookmarkModel.js
@@ -1,6 +1,13 @@
 function BookmarkModel() {
 	var that = this;
 
+	function generateGuid() {
+		return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+		    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+		    return v.toString(16);
+		});
+	}
+
 	function getAll() {
 
 	}
@@ -17,7 +24,7 @@ function BookmarkModel() {
 
 	}
 
-	function delete(bookmark) {
+	function destroy(bookmark) {
 
 	}
 
@@ -25,7 +32,7 @@ function BookmarkModel() {
 	that.get = get;
 	that.add = add;
 	that.update = update;
-	that.delete = delete;
+	that.destroy = destroy;
 }
 
 if (typeof module != 'undefined') {
